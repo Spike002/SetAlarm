258e00eaeb4945dc381490374e224ace0c73428b
Soravath Mengseng <soravathmengseng@Soravaths-MacBook-Air.local>
2019-04-14 18:48:31 -0400

    removed videos and added sound instead

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 //Designed and Developed
 //    by Soravath March 2019
 
+// Variable declearation
 var setAlarmbtn = document.getElementById('setAlarm');
 var vid = document.getElementById('myVideo');
-var pausevid = document.getElementById('pauseVideo');
 var opt = "";
+var sndAlarm = "";
 
+function playAlarmSound(setSound){
+  switch (setSound) {
+    case 'bomb':
+        new Audio('audios/BOMB_SIREN.mp3').play();
+      break;
+      case 'buzzer':
+        new Audio('audios/Loud_Alarm_Clock_Buzzer.mp3').play();
+        break;
+      case 'rooster':
+        new Audio('audios/Rooster Crowing.mp3').play();
+        break;
+      case 'tornado':
+        new Audio('audios/Tornado_Siren.mp3').play();
+        break;
+    default:
+
+  }
+}
 
 //get time format 00:00
 function getTime() {
@@ -27,25 +46,21 @@ function getTime() {
 
 function setAlarmTime(){
 
-  // all value from html are string
-
+//// Variable declearation
+// setting is string
   var settime= document.getElementById('time').value;
 
-  // // all Date number type
+  // all Date number type
    const now = new Date();
-   const day = now.getUTCDate();
+   const day = now.getUTCDay();
    var today = day.toString();
-
-
 
   if(opt !== today){
     console.log("It is wrong day");
     console.log(`${opt}!!!!${today}`);
-
-
   }else{
     if ( settime === getTime()){
-       vid.play();
+       playAlarmSound(sndAlarm);
        console.log('Wake up');
        console.log(opt);
        console.log(typeof opt);
@@ -53,23 +68,19 @@ function setAlarmTime(){
       console.log("It is wrong time");
       console.log(`Alarm Set${settime}!!....Time now${getTime()}`);
     }
-
   }
 }
 
 setAlarmbtn.addEventListener("click", () => {
   opt= document.getElementById('repeat').value;
-    if(opt === '0'){
-       setInterval(function () {
-       vid.play();
-     }, (5*1000)); //day*min*second*millisecond 24*60*60
+  sndAlarm = document.getElementById('audio').value;
+  console.log(sndAlarm);
+  console.log(typeof sndAlarm);
+    if(opt === 'rep'){
+        setInterval(function () {
+        playAlarmSound(sndAlarm);
+     }, (24*60*60*1000)); //day*min*second*millisecond 24*60*60*1000
     }else{
-      setInterval(setAlarmTime, 1000);
+      setInterval(setAlarmTime, 10*1000);
     }
-
-});
-
-
-pausevid.addEventListener("click", () =>{
-    vid.pause();
 });
